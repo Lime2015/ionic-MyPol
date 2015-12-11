6a25697c999c4f61ac9173cc1f85b28621de3a82
seongsan14 <eekek@naver.com>
2015-12-11 22:04:43 +0900

    kakao login test1

--- www/js/app.js
+++ www/js/app.js
@@ -24,7 +24,56 @@ angular.module('home', ['ionic'])
 })
 
 
-.controller('LoginCtrl', function($scope, $timeout, $ionicModal, Projects, $ionicSideMenuDelegate) {
+.controller('PopupLoginCtrl',function($scope, $ionicPopup, $timeout) {
 
+// Triggered on a button click, or some other target
+$scope.showPopup = function() {
+  $scope.data = {}
 
-});
+  // An elaborate, custom popup
+  var myPopup = $ionicPopup.show({
+    // template: '<input type="password" ng-model="data.wifi">',
+    templateUrl: 'kakao-login.html',
+    title: '', //'Enter Wi-Fi Password',
+    // subTitle: 'Please use normal things',
+    scope: $scope,
+    cssClass: 'css/layout.css;css/common.css;css/main.css'
+    // buttons: [
+    //   { text: 'Cancel' },
+    //   {
+    //     text: '<b>Save</b>',
+    //     type: 'button-positive',
+    //     onTap: function(e) {
+    //       if (!$scope.data.wifi) {
+    //         //don't allow the user to close unless he enters wifi password
+    //         e.preventDefault();
+    //       } else {
+    //         return $scope.data.wifi;
+    //       }
+    //     }
+    //   }
+    // ]
+  });
+  myPopup.then(function(res) {
+    console.log('Tapped!', res);
+  });
+  // $timeout(function() {
+  //    myPopup.close(); //close the popup after 3 seconds for some reason
+  // }, 3000);
+ };
+ // A confirm dialog
+ $scope.showConfirm = function() {
+   var confirmPopup = $ionicPopup.confirm({
+     title: 'Consume Ice Cream',
+     template: 'Are you sure you want to eat this ice cream?'
+   });
+   confirmPopup.then(function(res) {
+     if(res) {
+       console.log('You are sure');
+     } else {
+       console.log('You are not sure');
+     }
+   });
+ };
+
+})
