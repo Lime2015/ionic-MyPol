4d4f4862cedc0904d81a2c7768b27f2270029bdf
seongsan14 <eekek@naver.com>
2015-12-17 13:57:16 +0900

    test4

--- www/js/app.js
+++ www/js/app.js
@@ -24,7 +24,46 @@ angular.module('home', ['ionic'])
 })
 
 
-.controller('PopupLoginCtrl',function($scope, $ionicPopup, $timeout) {
+.controller('PopupLoginCtrl',function($http, $scope, $location, $ionicPopup, $timeout) {
+
+var clientId = "acd16274636c8807be1f6e2aa436ae52";
+var redirectUrl = "http://localhost:3000/oauth"
+var requestToken;
+var accessToken;
+
+$http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+$scope.kakaoLogin = function(){
+
+   // console.log(">>request kakaoLogin page")
+
+   https://kauth.kakao.com/oauth/authorize?client_id=acd16274636c8807be1f6e2aa436ae52&redirect_url=http://localhost:3000/oauth&response_type=code
+   // var ref = window.open("https://accounts.kakao.com/login?continue=https://kauth.kakao.com/oauth/authorize?client_id=" + clientId + "&redirect_url=" + redirectUrl + "&response_type=code", '_blank');
+   window.open("https://kauth.kakao.com/oauth/authorize?client_id=" + clientId + "&redirect_url=" + redirectUrl + "&response_type=code", '_self');
+   // ref.addEventListeneer('loadstart', function(event) {
+   //    if((event.url).startsWith(redirectUrl)) {
+   //       requestToken = (event.url).split("code=")[1];
+   //       $http({
+   //          method: "get",
+   //          url: "https://kauth.kakao.com/oauth/token",
+   //          data: "grant_type=authorization_code&client_id=" + clientId + "&redirect_url=" + redirectUrl + "&code=" + requestToken
+   //       })
+   //          .success(function(data) {
+   //             accessToken = data.access_token;
+   //             $location.path("/tab.dash");
+   //          })
+   //          .error(function(data, status) {
+   //             alert("error:" + data);
+   //          });
+   //       ref.close();
+   //    }
+   // });
+};
+
+// if(typeof String.prototype.startsWith != 'function') {
+//    String.prototype.startsWith = function(str) {
+//       return this.indexOf(str) == 0;
+//    };
+// }
 
 // Triggered on a button click, or some other target
 $scope.showPopup = function() {
